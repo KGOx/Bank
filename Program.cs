6d45e309cf9272aeda2ee9e15371d5d0f85129e6
Amnesis <temple_wizard@msn.com>
2024-10-12 12:01:09 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Créer une classe "personne" implémentant : Les propriétés publiques - String firstname, string lastname, datetime birthdate
+
+Personne personne = new();
+personne.firstname = "Gérard";
+personne.lastname = "Menvusa";
+personne.birthdate = new DateTime(1997, 5, 20);
+
+class Personne {
+    public string? firstname;
+    public string? lastname;
+    public DateTime birthdate;
+}
