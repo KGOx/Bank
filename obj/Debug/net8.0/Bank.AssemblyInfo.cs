d7916203d7165b6862afce1d7b1743edb7a5295c
Goossens Kevin <kevingoossens@me.com>
2025-01-25 09:06:33 +0100

    Validation du point 5 et du point 6

--- obj/Debug/net8.0/Bank.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bank.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bank")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7f3f25cae71eafefcfac3e6a08e987e04ad1183")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c9e8b842a639e98986a9ab0beb0c2a05b699088")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bank")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bank")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
